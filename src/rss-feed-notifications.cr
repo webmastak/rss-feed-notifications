e5e284d3070fb958753e68816e6464ef07f0644d
Andrew <web-meister@mail.ru>
2019-02-25 17:48:14 +0300

    Update app

--- src/rss-feed-notifications.cr
+++ src/rss-feed-notifications.cr
@@ -14,11 +14,6 @@ config = "#{set_refresh}\n#{set_summary}\n#{set_icon}\n#{set_url}\n"
 XDG_CONFIG_HOME = ENV["XDG_CONFIG_HOME"]? || File.expand_path("~/.config")
 PATH_LOCK = File.join(XDG_CONFIG_HOME, "rssfeed_lock")
 PATH_CONF = File.join(XDG_CONFIG_HOME, "rssfeed_config")
-PATH = File.join(XDG_CONFIG_HOME, "rssfeed_history")
-
-unless File.exists? PATH
-  File.write PATH, ""
-end
 
 unless File.exists? PATH_CONF
   File.write PATH_CONF, config
@@ -31,21 +26,10 @@ icon = conf.to_a.skip(2).first
 feed = RSS.parse conf.to_a.skip(3).first
 
 loop do
-  counter = 0
-  feed.items.each do |e|
-    File.open(PATH, "r+") do |file|
-      counter += 1
-      if counter == 1
-        file << "#{e.title}\n#{e.link}\n#{e.pubDate}"
-      end
-    end
-  end
 
-  h = File.read(PATH).split("\n").reverse
-  body = h.to_a.skip(2).first.split(" - ").first
-  url = h.to_a.skip(1).first
-  p_date = h.to_a.first.split("#{Time.now.year}").first
-  pub_date = p_date.split(" ").skip(1).first.to_i
+  e = feed.items.first
+  body = e.title.split(" - ").first
+  pub_date = e.pubDate.split(" ").skip(1).first.to_i
   current_date = Time.now.to_s("%e").to_i
   compare_date = pub_date == current_date || pub_date+1 == current_date ? 1 : 0
 
@@ -59,8 +43,8 @@ loop do
   if compare_date == 1
     unless File.exists? PATH_LOCK
       notification.update
-      Gio::AppInfo.launch_default_for_uri(url, nil)
-      File.write PATH_LOCK, p_date
+      Gio::AppInfo.launch_default_for_uri(e.link, nil)
+      File.write PATH_LOCK, e.pubDate
     end
   else
     File.delete PATH_LOCK if File.exists? PATH_LOCK
